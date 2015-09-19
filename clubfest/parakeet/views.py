452b19f1939050be2b1a69711a6acc391590f5dc
Liang Zhang <lz278@cornell.edu>
2015-09-19 13:28:59 -0400

    add search

--- clubfest/parakeet/views.py
+++ clubfest/parakeet/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 from .models import Map
+from .models import Club
+
 
 # Create your views here.
 def index(request, table_id=None):
@@ -9,9 +11,15 @@ def index(request, table_id=None):
         map_obj = Map.objects.get(id=1)
     except Map.DoesNotExist:
         pass
-    template = loader.get_template('map.html')
+    try:
+        club_object = Club.objects.get(id=2)
+    except Club.DoesNotExist
+        pass
+    #template = loader.get_template('map.html')
+    template = loader.get_template('search.html')
     context = RequestContext(request, {
         'map': map_obj,
+        'club': club_obj,
         'selected_table': table_id
     })
     return HttpResponse(template.render(context))
