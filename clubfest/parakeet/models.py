fb3f08792d01b48ab2abfc98ede4de8f13f31420
ewang <ewang@ewang-350V5C.(none)>
2015-09-18 23:25:05 -0700

    models

--- clubfest/parakeet/models.py
+++ clubfest/parakeet/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Club(models.Model):
+    table = models.ForeignKey(Table)
+    category = models.CharField(max_length=256)
+
+class Table(models.Model):
+    x = models.IntegerField()
+    y = models.IntegerField()
+    club = models.ForeginKey(Club)
+
+class Map(models.Model):
+    height = models.IntegerField()
+    width = models.IntegerField()
+    tables = ArrayField(
+        ArrayField(
+            models.IntegerField()
+        ),
+    )
