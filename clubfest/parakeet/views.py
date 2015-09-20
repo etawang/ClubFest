604ff17d26e1a244fd08f35b1724f184cd3cdd47
Liang Zhang <lz278@cornell.edu>
2015-09-19 21:59:57 -0400

    search_more

--- clubfest/parakeet/views.py
+++ clubfest/parakeet/views.py
@@ -69,37 +69,36 @@ def index(request, table_id=None):
     template = loader.get_template('map.html')
     request_dict = {}
     request_dict['map'] = full_map_for_map(map_obj)
-    if 'club_search_form' is in request.POST:
-        form2 = SearchClubForm(request.POST)
-        if form2.is_valid():
-            club_name=form2.cleaned_data['club_name']
-            club_category =form2.cleaned_data['club_category']
-            print club_name+" "+club_category #comment out afterwards
-            if club_name !="":
-                #make sure upper and lower case all work
-                thisclub=Club.objects.filter(club_name=club_name)
-                if thisclub and thisclub[0].table_id>=0:
-                    table_id=thisclub[0].table_id
-                    if club_category=="epty" or club_category==thisclub[0].category:
-                        request_dict['highlighted_club']=table_id
-                        request_dict['message']="The table ID for the club is "+ str(table_id)
-                    elif thisclub[0].category!=club_category:
-                        request_dict['message']="The club you are searching is not in the given category. Please check!"
-                else:
-                    request_dict['message']="This club cannot be found."
-            elif club_category!="":
-                searchclubs=Club.objects.filter(category=club_category)
-                if searchclubs:
-                    selected=[]
-                    clubnames=""
-                    for eachclub in searchclubs:
-                        clubnames+=eachclub.club_name+" "
-                        this_tableid=eachclub.table_id
-                        selected.append(this_tableid)
-                    request_dict['selected']=selected
-                    print clubnames
-                    request_dict['message']="The clubs to be checked out are highlighted: "+clubnames
-        request_dict['form2']=form2
+    form2 = SearchClubForm(request.POST)
+    if form2.is_valid():
+        club_name=form2.cleaned_data['club_name']
+        club_category =form2.cleaned_data['club_category']
+        print club_name+" "+club_category #comment out afterwards
+        if club_name !="":
+            #make sure upper and lower case all work
+            thisclub=Club.objects.filter(club_name=club_name)
+            if thisclub and thisclub[0].table_id>=0:
+                table_id=thisclub[0].table_id
+                if club_category=="epty" or club_category==thisclub[0].category:
+                    request_dict['highlighted_club']=table_id
+                    request_dict['message_1']="The table ID for the club is "+ str(table_id)
+                elif thisclub[0].category!=club_category:
+                    request_dict['message_1']="The club you are searching is not in the given category. Please check!"
+            else:
+                request_dict['message_1']="This club cannot be found."
+        elif club_category!="":
+            searchclubs=Club.objects.filter(category=club_category)
+            if searchclubs:
+                selected=[]
+                clubnames=""
+                for eachclub in searchclubs:
+                    clubnames+=eachclub.club_name+" "
+                    this_tableid=eachclub.table_id
+                    selected.append(this_tableid)
+                request_dict['selected']=selected
+                print clubnames
+                request_dict['message_1']="The clubs to be checked out are highlighted: "+clubnames
+    request_dict['form2']=form2
 
     if table_id:
         table_id = int(table_id)
